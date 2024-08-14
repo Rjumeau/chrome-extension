27e9d8f49aef6cfc3097e45c9cf9b4c96bea1210
Romain Jumeau <romain.jumeau@hotmail.fr>
2024-08-14 14:37:59 +0200

    done

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,8 @@
-function sendCheesifyMsg() {
-  // TODO: Write a function to send a message to the active tab to 'cheesify' it
-}
-
-// TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
+const button = document.querySelector('#click')
+button.addEventListener('click', (e) => {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "select-modal"}, function(response) {
+      console.log(response);
+    });
+  });
+})
